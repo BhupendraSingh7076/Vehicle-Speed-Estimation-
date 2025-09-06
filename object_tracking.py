e15876420ba5d3812142933133972642f03c1666
BhupendraSingh7076 <bhupendrasinghbhati7076@gmail.com>
2025-09-06 15:02:33 +0530

    Create object_tracking.py

---
+++ object_tracking.py
@@ -0,0 +1,13 @@
+What we’ll do with your notebook
+
+Extract reusable code
+
+Model loading → detector.py
+
+Vehicle tracking & ID assignment → tracker.py
+
+Speed calculation logic → speed_estimator.py
+
+CSV writing + visualization → utils.py
+
+Keep notebook
